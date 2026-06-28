1e788f689d803848d37ec5c578ec104bed4c1156
Manuel Van Dyck <uitsmijter-trekhaken-1j@icloud.com>
2026-06-28 21:06:56 +0200

    Fix: juiste antwoord tonen in Toets (netjes, geen JSX-fout)

--- src/pages/Toets.tsx
+++ src/pages/Toets.tsx
@@ -292,12 +292,11 @@ export default function Toets() {
 
             {/* Toon juist antwoord na feedback */}
             {antwoordStatus === 'feedback' && (
-              <div className="mt-4 p-3 bg-blue-50 border border-blue-200 rounded-lg">
+              <div className="bg-blue-50 border border-blue-200 rounded-lg p-3 mb-4">
                 <span className="text-sm font-medium text-blue-700">Juiste antwoord: </span>
                 <span className="text-sm text-blue-800 font-mono" dangerouslySetInnerHTML={{ __html: renderKatex('$' + huidigeVraag.juist_antwoord + '$') }} />
               </div>
             )}
-          </>
 
             {/* Actieknoppen */}
             <div className="flex gap-3">
